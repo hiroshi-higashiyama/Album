451f8cac7918ab563b45ebc13a219eddf44feb0f
hiroshi <s20840011@gmail.com>
2021-07-24 11:05:25 +0900

    document.title修正

--- album_vue/src/views/Home.vue
+++ album_vue/src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home">
-    <div class="hero is-fullheight" style="background-image: url('StockSnap_RTUVC7BMZI.jpg')">
+    <div
+      class="hero is-fullheight"
+      style="background-image: url('StockSnap_RTUVC7BMZI.jpg')"
+    >
       <div class="hero-body">
         <div class="container has-text-centered">
           <h1 class="home title mb-6">Welcome to Photo Storage</h1>
           <h2 class="home subtitle">The best image</h2>
           <h2 v-if="$store.state.isAuthenticated">
-                <router-link to="/posts" class="button is-success mt-5"
-                  >Start</router-link
-                >
+            <router-link to="/posts" class="button is-success mt-5"
+              >Start</router-link
+            >
           </h2>
         </div>
       </div>
@@ -19,11 +22,14 @@
 <script>
 export default {
   name: "Home",
+  mounted() {
+    document.title = "Photo Storage";
+  },
 };
 </script>
 
 <style lang="scss">
-  .hero {
+.hero {
   @extend .hero;
   background-size: cover;
   background-position: center;
