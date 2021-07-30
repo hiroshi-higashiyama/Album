02403a2279c3173e18a29b236a05367b5ae6e104
hiroshi <s20840011@gmail.com>
2021-07-31 04:03:56 +0900

    main.js修正

--- album_vue/src/main.js
+++ album_vue/src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
-// https://album-drf-vue.herokuapp.com
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL
 
 createApp(App).use(store).use(router, axios).mount('#app')
